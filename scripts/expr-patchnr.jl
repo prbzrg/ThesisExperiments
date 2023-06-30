7aeb5e0ddfcfc7caa2bd5b3463663a155139a6ce
Hossein Pourbozorg <prbzrg@gmail.com>
2023-06-30 21:31:35 +0330

    sets

--- scripts/expr-patchnr.jl
+++ scripts/expr-patchnr.jl
@@ -29,9 +29,9 @@ allparams = Dict(
     # "tspan_end" => [1, 8],
 
     # ICNFModel
-    "n_epochs" => 2^5,
+    "n_epochs" => 2^3,
     # "n_epochs" => 2,
-    "batch_size" => 2^15,
+    "batch_size" => 2^12,
     # "batch_size" => 32,
 )
 dicts = dict_list(allparams)
