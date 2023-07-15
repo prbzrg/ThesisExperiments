409719eeed2a21f3e63362fd8d23fa83701c7538
Hossein Pourbozorg <prbzrg@gmail.com>
2023-07-15 10:29:12 +0330

    hidden rate 8

--- scripts/expr-patchnr.jl
+++ scripts/expr-patchnr.jl
@@ -16,7 +16,7 @@ allparams = Dict(
     # "p_s" => [4, 6, 8],
 
     # nn
-    "n_hidden_rate" => 2,
+    "n_hidden_rate" => 8,
     "arch" => "Dense-ML",
     # "arch" => "Dense",
     # "back" => "Lux",
