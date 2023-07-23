034106393d86d175e5ad513c4dc84deae7b84750
Hossein Pourbozorg <prbzrg@gmail.com>
2023-07-23 13:55:25 +0330

    up

---
+++ scripts/update_pkgs.jl
@@ -0,0 +1,9 @@
+ENV["PYTHON"] = raw"C:\Program Files\Python39\python.exe"
+
+using Pkg
+
+Pkg.precompile(; strict = true)
+Pkg.update()
+Pkg.precompile(; strict = true)
+Pkg.build()
+Pkg.precompile(; strict = true)
