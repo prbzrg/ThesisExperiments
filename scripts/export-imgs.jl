c0f0dd3b27b060a8d99cf76baf762c064aa563c2
Hossein Pourbozorg <prbzrg@gmail.com>
2023-07-29 00:18:54 +0330

    new plot for images

--- scripts/export-imgs.jl
+++ scripts/export-imgs.jl
@@ -5,17 +5,25 @@ include(scriptsdir("import_pkgs.jl"))
 
 df = collect_results(datadir("patchnr-sims"))
 
-for i in 1:size(df, 1)
-    save(
-        plotsdir("patchnr-sims-imgs", "$(i)_result.png"),
-        convert.(Gray, stan_img(df[i, "res_img"])),
+res_sz = (362 * 2 * 3, 362 * 2)
+r_dgt = 4
+
+for i in axes(df, 1)
+    f = Figure(; resolution = res_sz)
+    ax1 = Makie.Axis(
+        f[1, 1];
+        title = "Filtered Back-projection",
+        subtitle = "PSNR: $(round(df[i, "fbp_a_psnr"]; digits=r_dgt)), SSIM: $(round(df[i, "fbp_a_ssim"]; digits=r_dgt)), MSSSIM: $(round(df[i, "fbp_a_msssim"]; digits=r_dgt))",
     )
-    save(
-        plotsdir("patchnr-sims-imgs", "$(i)_fbp.png"),
-        convert.(Gray, stan_img(df[i, "fbp_img"])),
+    image!(ax1, rotr90(df[i, "fbp_img"]))
+    ax2 = Makie.Axis(f[1, 2]; title = "Ground Truth")
+    image!(ax2, rotr90(df[i, "gt_x"]))
+    ax3 = Makie.Axis(
+        f[1, 3];
+        title = "Result",
+        subtitle = "PSNR: $(round(df[i, "a_psnr"]; digits=r_dgt)), SSIM: $(round(df[i, "a_ssim"]; digits=r_dgt)), MSSSIM: $(round(df[i, "a_msssim"]; digits=r_dgt))",
     )
-    save(
-        plotsdir("patchnr-sims-imgs", "$(i)_goal.png"),
-        convert.(Gray, stan_img(df[i, "gt_x"])),
-    )
+    image!(ax3, rotr90(df[i, "res_img"]))
+    save(plotsdir("patchnr-sims-imgs", "$(i)_plot.svg"), f)
+    save(plotsdir("patchnr-sims-imgs", "$(i)_plot.png"), f)
 end
