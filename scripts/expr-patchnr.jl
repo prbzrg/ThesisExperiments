038e52991251e5a2f43364f41436d0f7bd579c45
Hossein Pourbozorg <prbzrg@gmail.com>
2023-08-18 12:15:10 +0330

    ne

--- scripts/expr-patchnr.jl
+++ scripts/expr-patchnr.jl
@@ -20,7 +20,7 @@ const allparams = Dict(
     # "p_s" => [4, 6, 8],
     "naug_rate" => 1 + (1 / 36),
     "rnode_reg" => eps_sq[4],
-    "steer_reg" => eps_sq[5],
+    "steer_reg" => eps_sq[4],
 
     # nn
     "n_hidden_rate" => 0,
@@ -30,7 +30,7 @@ const allparams = Dict(
     "back" => "Flux",
 
     # construct
-    "tspan_end" => 9,
+    "tspan_end" => 17,
     # "tspan_end" => [1, 4, 8, 32],
 
     # ICNFModel
