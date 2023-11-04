6948ee53f456d5a1814b796fda4fb6fd21999c0d
Hossein Pourbozorg <prbzrg@gmail.com>
2023-11-04 12:30:16 +0330

    up

--- scripts/expr-patchnr.jl
+++ scripts/expr-patchnr.jl
@@ -25,7 +25,7 @@ const allparams = Dict(
     "rnode_reg" => eps_sq[4],
     "steer_reg" => eps_sq[4],
     "ode_reltol" => eps_sq[3],
-    "tspan_end" => 9,
+    "tspan_end" => 13,
 
     # nn
     "n_hidden_rate" => 0,
